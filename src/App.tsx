73df597b6b399fe2940e1178bdc9ab29dd59c366
DDMeaqua <lzlloveyou@vip.qq.com>
2024-06-04 17:16:11 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+function App() {
+
+  return (
+    <div className='bg-black w-full h-screen flex flex-col justify-center items-center'>
+      <img src="/logo.png" alt="" />
+      <h1 className='text-3xl font-semibold text-white mt-8'>Vite + React + tailwindcss</h1>
+    </div>
+  )
+}
+
+export default App
